2f69e48695f87855ec1e152b79c715e23d9682ec
Andrew Oliveira <andrewhenriquef@gmail.com>
2024-07-14 13:10:48 -0300

    (=) Reorganize html and broadcast channel

--- app/helpers/robots_helper.rb
+++ app/helpers/robots_helper.rb
@@ -1,2 +1,9 @@
 module RobotsHelper
+  def render_robot(robot)
+    content_tag :div, '', class: "robot #{robot.face}"
+  end
+
+  def robot_in_board_index_position?(robot, row, col)
+    row == robot.axis_y && col == robot.axis_x
+  end
 end
